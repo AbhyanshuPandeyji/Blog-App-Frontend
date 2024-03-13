64683378d0a7eceb86e1225527a85c7160f83238
Abhyanshu Pandey <abhyanshupandeyji@gmail.com>
2024-03-13 15:54:37 +0530

    trying to host the project for now , fixed some data intake errors and , now know how to use the mongodb url , need encoding try it in ecommerce app , and know how to create the url of the and connection to the database

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,104 @@
-import { useState } from 'react'
-import Navbar from './components/navbar/Navbar';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 // import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
+// packages import 
+// import { useState } from 'react'
+import Navbar from './components/navbar/Navbar.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+  Navigate
+
+} from "react-router-dom";
+
+
+
+// component import
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from './pages/Register/Register.jsx';
+
+
+
+
+const Layout = () => {
 
   return (
-    <>
-      <div>
-        <Navbar/>
+    <div className="">
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+
+}
+
+
+// const ProtectedRoute = ({ children }) => {
+//   if (!currentUser) {
+//     return <Navigate to="/login" />
+//   }
+
+//     // children meaning the component passed inside the protected route
+//     // it works as a function , taking the component as an  argument and returning 
+//     // based on if the current user exist in local storage , if not then go to login
+//     // it doesn't affect or manipulate the component , rather it just  redirects it
+//     return children;
+// }
+
+
+
+// remember only one page can be shown at one link , only number of components can change within it'
+// which if not on every page can be put into specific pages , if  needed in every page like navbar , 
+// will be put into the layout to render on all the pages , don't think to put 
+// multiple components in one , its routing not page rendering , different component can be put into the 
+// the main component render on that url / routing link , and can also be used through the stored and saved data
+// into the backend to access different routes with different data with stored state
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Layout />),
+    // these are the components that are going to pass in the layout to render in outlet
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/blogs",
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path: "/Login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+])
+
+
+
+
+
+
+function App() {
+
+  return (
+    <div>
+      {/* <div>
+        <Navbar />
         <p>Hello World</p>
         <h1>Hello World</h1>
         <h2>Hello World</h2>
-      </div>
-    </>
+      </div> */}
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,81 @@
+// import React from 'react'
+
+import { useEffect, useState } from "react";
+
+
+// functions import 
+import { makeRequest } from "../../axios.js";
+
+const Home = () => {
+
+  const [data, setData] = useState()
+
+
+  useEffect(() => {
+
+    const response = async () => {
+      await makeRequest.get('/user/')
+        // response.data is way to fetch data with axios , as res.json() in fetch request
+        .then((response) => setData(response.data))
+    }
+
+    response();
+  }, [])
+
+
+  if (data == null) return <div>Loading...</div>;
+  if (data == undefined) return <div>Loading...</div>;
+  if (!data) return null;
+  console.log(data);
+  // console.log(typeof data);
+
+  // for (let i in data) {
+  //   console.log(data[i].role);
+  // }
+
+
+
+  return (
+    <div>
+      Home Page
+      {/* {data} */}
+      {/* {data.map((data, index) => (
+        <div key={index}>
+          {data}
+        </div>
+      ))
+
+      } */}
+      <table className="container">
+        <thead>
+          <tr>
+            {/* <th>Role</th> */}
+            <th>Name</th>
+            {/* <th>Age</th> */}
+            {/* <th>Occupation</th> */}
+            <th>Email</th>
+            <th>Password</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.user.map((datas, index) => (
+            <tr key={index}>
+              {/* <td>{datas.role}</td> */}
+              <td>{datas.name}</td>
+              {/* <td>{datas.age}</td> */}
+              {/* <td>{datas.occupation}</td> */}
+              <td>{datas.email}</td>
+              <td>{datas.password}</td>
+              <td>{datas.username}</td>
+
+            </tr>
+          ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Home;
