a6a48d030cbec057851b3ec6abf588e13c7476bf
Abhyanshu Pandey <abhyanshupandeyji@gmail.com>
2024-06-09 15:40:19 +0530

    added the redux , created store and created userreducer , and added the toastify , only functionality added and no designing

---
+++ src/routes/Routing.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+  Navigate
+
+} from "react-router-dom";
+
+
+// components import 
+import Home from "../pages/Home/Home.jsx";
+import Navbar from "../components/navbar/Navbar.jsx"
+
+// in the other app instead of providing the router provider its using the browser router , which is the main component to show the
+// routes in the app , in the main file , so it doesn't need to explicitly say router provider it can directly uses the routes and the children elements
+const Routing = () => {
+  const Layout = () => {
+
+    return (
+      <div className="">
+        <Navbar />
+        <Outlet />
+      </div>
+    )
+  };
+
+
+  // in this
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (<Layout />),
+      // these are the components that are going to pass in the layout to render in outlet
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        // {
+        //   path: "/blog",
+        //   element: <BlogCardGrid />
+        // }
+      ]
+    },
+    // {
+    //   path: "/Login",
+    //   element: <Login />
+    // },
+    // {
+    //   path: "/register",
+    //   element: <Register />
+    // },
+  ])
+
+
+  return (<RouterProvider router={router} />)
+
+}
+
+export default Routing;
